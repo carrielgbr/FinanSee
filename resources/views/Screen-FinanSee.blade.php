cf4bdcab2e3f06824ce19472a2ef68dea6f8ddab
gbrcarriel <gabriel313g@gmail.com>
2023-11-02 00:11:49 -0300

    view Home atualização

---
+++ resources/views/Screen-FinanSee.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>FinanSee</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Image/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Image/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+</head>
+<body>
+    <header>
+        <H1>FinanSee</H1>
+            <!-- varivel php do nome do usuario -->
+            <!-- Botao sair -->
+    </header>
+    <Section>
+        <div>
+            <form action="">
+                <input type="text" name="" id="">
+                inpu
+            </form>
+        </div>
+    </Section>
+            
+</body>
+</html>
--- resources/views/Screen-Home.blade.php
+++ resources/views/Screen-Home.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
-    <title>FinanSee</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="Image/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="Image/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-</head>
-<body>
-    <H1>FinanSee</H1>
-        <form action="" method="get">
-            <P>LOGIN</P>
-            <input type="email" name="" id="" placeholder="E-MAIL">
-            <input type="password" name="" id="" placeholder="SENHA"><br>
-            <input type="submit" value="CADASTRO">
-            <input type="submit" value="LOGIN">
-        </form>
-</body>
+    <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/app.css">
+            <title>FinanSee</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="Image/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="Image/favicon-16x16.png">
+            <link rel="manifest" href="/site.webmanifest">
+        </head>
+        <body>
+            <header id="titulo">
+                <h1>FinanSee</h1>
+            </headr>
+            <section class="login_cad">
+                <form action="" method="get">
+                    <P>LOGIN</P>
+                    <div class="textBox">
+                        <input type="email" name="" id="" placeholder="E-MAIL">
+                        <input type="password" name="" id="" placeholder="SENHA">
+                    </div>
+                    <input type="submit" value="LOGIN" class="login"><br>
+                    <a href="" class="link_page" >Cadastrar</a>
+                </form>
+            </section>
+        </body>
 </html>
